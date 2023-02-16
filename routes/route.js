cce91962d98de98d388092164bb2dd2194fd72c9
Nilesh Kumar <nileshkumar@geekyants.com>
2023-02-17 02:30:03 +0530

    message change

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 const Router = require("express").Router();
 
 Router.route("/nilesh").get((req, res) => {
-  res.send("Bhaiya pranam!");
+  res.send("Bhaiya ji ko pranam!");
 });
 
 module.exports = { Router };
