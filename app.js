3ab20393d05f54ebb8c5f62a73e3205bc29eb697
Nilesh Kumar <nileshkumar@geekyants.com>
2023-02-16 11:49:41 +0530

    commented options for https-connection

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const path = require("path");
 const cors = require("cors");
 const app = express();
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, "./ssl/key.pem")),
-  cert: fs.readFileSync(path.join(__dirname, "./ssl/cert.pem")),
-};
+// const options = {
+//   key: fs.readFileSync(path.join(__dirname, "./ssl/key.pem")),
+//   cert: fs.readFileSync(path.join(__dirname, "./ssl/cert.pem")),
+// };
 
 //const server = https.createServer(options, app);
 const server = http.createServer(app);
